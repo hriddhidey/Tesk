33221c33d046a0fb873e7e2d69b93b03e024619a
Hriddhi Dey <hriddhidey@gmail.com>
2016-04-24 06:34:22 +0530

    Added files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+var data="";
+
+function startDictation() {
+
+  if (window.hasOwnProperty('webkitSpeechRecognition')) {
+
+    var recognition = new webkitSpeechRecognition();
+
+
+    
+    recognition.continuous = false;
+    recognition.interimResults = false;
+    
+    
+    recognition.lang = "en-US";
+    recognition.start();
+    
+    recognition.onresult = function(e) {
+
+       //console.log('Hello.');
+      
+      //document.getElementById('translatedText').innerHTML = e.results[0][0].transcript;
+      data = e.results[0][0].transcript;
+      recognition.stop(); 
+      writeTranslated()
+      
+      //document.getElementById('labnol').submit();
+    }
+    
+    recognition.onerror = function(e) {
+      recognition.stop();
+      
+    }
+  }
+}
+
+function writeTranslated() {
+	console.log(data);
+	data.charAt(0).toUpperCase();
+	console.log(data);
+	data.concat(". ");
+	console.log(data);
+	document.getElementById("translated").innerHTML+=data;
+	console.log(data);
+}
