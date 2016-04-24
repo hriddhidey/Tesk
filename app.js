66e4812a2a97769eae73740b3681c75546eeb3a4
sonal2694 <nikki2694@gmail.com>
2016-04-24 12:22:26 +0530

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("openFile").addEventListener('change',function(){
-	var fr=new FileReader();
-	fr.onload=function(){
-		document.getElementById("fileContents").textContent=this.result;
-	}
-	fr.readAsText(this.files[0]);
-})
